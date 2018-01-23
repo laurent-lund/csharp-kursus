29e0b28a8cfdc429971aa75a3055a03dfa8d41fc
admin <admin@TA-8211-04.kursdom.net>
2018-01-23 10:26:07 +0100

    Add project files.

---
+++ Module02_HelloWorld/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Module02_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.Write('H');
+            Console.Write('e');
+            Console.Write('l');
+            Console.Write('l');
+            Console.Write('o');
+            Console.Write(' ');
+            Console.Write('W');
+            Console.Write('o');
+            Console.Write('r');
+            Console.Write('l');
+            Console.Write('d');
+            Console.WriteLine('!');
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            String HW = "Hello world.";
+            int length;
+
+            length = HW.Length;
+                Random rand = new Random();
+
+            for (int i = 0; i < HW.Length; i++)
+            {
+                int farve;
+                farve = rand.Next(11);
+                Console.ForegroundColor = (System.ConsoleColor)farve;
+                Console.Write(HW[i]);
+            }
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+
+        static void DebugDemo()
+        {
+
+            int i = 10;
+            int u = 20;
+            u = u + i;
+            string name = "Mikkel";
+            System.Console.WriteLine(name);
+            string upperName = name.ToUpper();
+            System.Console.WriteLine(upperName);
+
+            for (int x = 0; x < 3; x++)
+            {
+                u += x;
+            }
+
+            Test1(5);
+
+        }
+
+        static void Test1(int i)
+        {
+            i++;
+            Test2(i);
+        }
+
+        static void Test2(int i)
+        {
+            i--;
+            Test3(i);
+        }
+
+        static void Test3(int i)
+        {
+            i++;
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialiser
+            Int32 heltal = 10;
+
+            //Add 1
+            heltal++;
+
+            //Subtract 1
+            heltal--;
+
+            // Add 20
+            heltal += 20;
+
+            //Spill the beans
+            Console.WriteLine("Heltal:" + heltal);
+
+            //Initialize
+            double kommatal = 12.5;
+
+            //Add 1
+            kommatal++;
+
+            //Subtract 1
+            kommatal--;
+
+            // Multiply by 2
+            kommatal *= 2;
+
+            //Spill the beans
+            Console.WriteLine("Kommatal:" + kommatal);
+
+            //Enums
+            FilTyper ft = FilTyper.csv;
+
+            //Spill the beans
+            Console.WriteLine("Filetype:" + ft);
+
+            Console.WriteLine("Filetype (value):" + ft);
+
+            //Dates
+            DateTime dato = DateTime.Now;
+
+            Console.WriteLine("Now is:" + dato);
+            Console.WriteLine("Now is:" + dato.ToString());
+            Console.WriteLine("Now is:" + dato.ToShortTimeString());
+            Console.WriteLine("Now is:" + dato.ToLongDateString());
+
+            //Structs
+            Person p = new Person();
+            p.Navn = "Mikkel";
+            p.Id = 42;
+            Console.WriteLine("Name is:" + p.Navn);
+            Console.WriteLine("Id is:" + p.Id);
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+
+}
+      
+    }
+    enum FilTyper { csv, pdf, txt }
+    //Structs
+    struct Person
+    {
+        public string Navn;
+        public int Id;
+    };
+}
