cfb23e4dd1d3eb5abf0d1e66f1ae6bdaa8063acd
laurent-lund <laurentlund68@gmail.com>
2018-02-05 14:16:25 +0100

    Nu med Person og egenskaber

---
+++ Person/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+
+//        private string fornavn;
+//        private string efternavn;
+//        private string fuldtnavn;
+
+        //Automatisk egenskab - prop+TAB
+        public string Fornavn { get; set; }
+
+        //Fuld egenskab - propfull+TAB
+        private string efternavn;
+        public string Efternavn
+        {
+            get { return efternavn; }
+            set { efternavn = value; }
+        }
+
+        // Ingen egenskab
+        public string FuldtNavn()
+        {
+            return Fornavn + ' ' + Efternavn;
+        }
+
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opret ny 'person' p1.
+            Person p1 = new Person();
+
+            //Sæt for- og efternavn.
+            p1.Fornavn = "Anders";
+            p1.Efternavn = "And";
+
+            Console.WriteLine("Fornavn   : "+p1.Fornavn);
+            Console.WriteLine("Efternavn : "+p1.Efternavn);
+            Console.WriteLine("Fuldt navn: " + p1.FuldtNavn());
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
