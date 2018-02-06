fc65c26d67c847203e86c9c1b65a95266670040a
laurent-lund <laurentlund68@gmail.com>
2018-02-06 15:14:13 +0100

    20180206 1513 Nu med mere terning

--- Terning/Program.cs
+++ Terning/Program.cs
@@ -10,6 +10,28 @@ namespace Terning
     {
         static void Main(string[] args)
         {
+            // Opret ny terning, og lad t1 pege på den.
+            Terning t1 = new Terning();
+
+            // Ryst t1 og vis resultatet.
+            t1.Ryst();
+            t1.Skriv();
+
+            // Opret ny terning, der som udgangspunkt viser fire.
+            Terning t2 = new Terning(4);
+
+            // Vis terningen, og se det er korrekt at der er fire øjne.
+            t2.Skriv();
+
+        
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
         }
     }
 }
--- Terning/Terning.cs
+++ Terning/Terning.cs
@@ -8,6 +8,63 @@ namespace Terning
 {
     class Terning
     {
+        private int værdi;
+        private static System.Random rnd = new Random();
+
+        // Constructor - Terning rystes når den skabes.
+        public Terning()
+        {
+            this.Ryst();
+        }
+
+        public Terning(int antaløjne)
+        {
+            this.værdi = antaløjne;
+        }
+
+
+        public int Værdi
+        {
+            get { return værdi; }
+            set
+            {
+                if (1 > value)
+                {
+                    værdi = 1;
+                }
+                else if (value > 6)
+                {
+                    værdi = 6;
+                }
+                else
+                {
+                    værdi = value;
+                }
+            }
+        }
+
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(7);
+        }
+
+        public virtual void Skriv()
+        {
+            Console.WriteLine("[" + værdi + "]");
+        }
+    }
+
+    class LudoTerning : Terning
+    {
+        public bool ErGlobus()
+        {
+            return (this.Værdi == 3);
+        }
+
+        public bool ErStjerne()
+        {
+            return (this.Værdi == 5);
+        }
 
     }
 }
