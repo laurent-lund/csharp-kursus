e1b9229e431c2fa0d353ae449b4a660ed08d85ce
laurentlund <laurentlund68@gmail.com>
2018-02-19 15:32:15 +0100

    20180219 1531 Lister (stub)

---
+++ Lister/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
