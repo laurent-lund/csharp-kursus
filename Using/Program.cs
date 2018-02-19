6b5b139b4fb1f9142aae909c2cbe28737892564c
laurentlund <laurentlund68@gmail.com>
2018-02-19 14:44:27 +0100

    20180219 1444 file read med Using

---
+++ Using/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // File read UDEN 'Using'.
+            Console.WriteLine("File read uden Using.");
+            StreamReader stream = System.IO.File.OpenText(@"x:\dyrenavne.txt");
+            while (stream.Peek() != -1)
+            {
+                string navn = stream.ReadLine();
+                Console.WriteLine(navn);
+            }
+            stream.Close();
+            stream = null;
+            Console.WriteLine("");
+
+
+            // File read MED 'Using'.
+            Console.WriteLine("File read med Using.");
+            using (StreamReader sr = File.OpenText(@"x:\dyrenavne.txt"))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Using/Streamread.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Using
+{
+    public class Streamread
+    {
+ //       StreamReader stream = System.IO.File.OpenText(@"x:\dyrenavne.txt");
+ //       while (Stream.Peek() != -1)
+	//{
+
+	//}
+
+
+    }
+}
