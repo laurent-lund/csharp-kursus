cc60f584d12c44b43250af112f31b07fad0f633e
laurentlund <laurentlund68@gmail.com>
2018-02-20 09:42:02 +0100

    20180220 Kortbunke

---
+++ CollectionKort/Kort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionKort
+{
+    public class Kort
+    {
+        public string Kulør { get; set; }
+        public int Værdi { get; set; }
+
+        public override string ToString()
+        {
+            return this.Kulør + " " + this.Værdi;
+        }
+
+        public class Bunke
+        {
+            // Opret en stak der kan holde Kort.
+            private Stack<Kort> bunke = new Stack<Kort>();
+
+            // Metode til at lægge et kort på stakken.
+            public void TilføjKort(Kort k)
+            {
+                bunke.Push(k);
+            }
+
+            // Metode til at fjerne øverste kort fra stakken.
+            public Kort FjernKort()
+            {
+                // Hvis der er kort på stakken, returner øverste
+                if (bunke.Count > 0)
+                    return bunke.Pop();
+                // ellers returner null.
+                return null;
+            }
+
+            // Metode til at vise de kort der ligger på stakken.
+            public void visStak()
+            {
+                foreach (var kort in bunke)
+                {
+                    Console.WriteLine(kort);
+                }
+            }
+        }
+    }
+}
+
---
+++ CollectionKort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CollectionKort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skab en ny tom bunke.
+            Kort.Bunke b = new Kort.Bunke();
+
+            // Tilføj tre kort.
+            b.TilføjKort(new Kort() { Kulør = "Spar", Værdi = 2 });
+            b.TilføjKort(new Kort() { Kulør = "Hjerter", Værdi = 14 });
+            b.TilføjKort(new Kort() { Kulør = "Ruder", Værdi = 7 });
+
+            // Vis hvad der er i bunken.
+            b.visStak();
+
+            // Fjern øverste kort.
+            var k = b.FjernKort();
+
+            // Vis øverste kort - ToString metoden er overloadet.
+            System.Console.WriteLine();
+            System.Console.WriteLine(k);
+            System.Console.WriteLine();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
