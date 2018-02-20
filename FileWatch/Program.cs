5f5faa49b44eab6886560ffc1b79104043b20b33
laurentlund <laurentlund68@gmail.com>
2018-02-20 14:05:38 +0100

    20180220 1405 FileWatch

---
+++ FileWatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reager på ændringer af filer af typen *.txt i "C:\temp\".
+            FileSystemWatcher w = new FileSystemWatcher(@"c:\temp\*.txt");
+
+            // Sæt igang med at lytte.
+            w.EnableRaisingEvents = true;
+
+            // Filtrering så vi ikke spammes af hændelser.
+            w.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+
+            // Tilføj 'changed' delegate
+            w.Changed += W_Changed;
+        }
+
+        private static void W_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
